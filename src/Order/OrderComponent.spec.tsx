0cc1b4895f67f1ac5b3717b52ad9b384b8d9c914
minhaxon <minh.nguyenvan@axonactive.com>
2021-02-19 16:54:20 +0700

    feat: Adding testing for post saga file

--- src/Order/OrderComponent.spec.tsx
+++ src/Order/OrderComponent.spec.tsx
@@ -1,29 +1,29 @@
-import React from "react"
-import { OrderComponent } from "."
-import { testRender } from "../test"
-import { screen } from '@testing-library/dom'
-import * as redux from 'react-redux'
+import React from "react";
+import { OrderComponent } from ".";
+import { testRender } from "../test";
+import { screen } from "@testing-library/dom";
+import * as redux from "react-redux";
 
-describe('<OrderComponent />', () => {
-    it('shoud render Order page not crashing', () => {
-        jest.useFakeTimers()
-        testRender(<OrderComponent/>)
-        setTimeout(() => {
-            expect(screen.getByText('Order Page')).toBeTruthy()
-        }, 1000)
-        jest.runAllTimers()
-    })
-    it('load login state in Order component', () => {
-        const login = {
-            isLoading: false,
-            username: '',
-            token: '',
-            error: ''
-        }
-        const loginSelectorSpy = jest.spyOn(redux, 'useSelector')
-        loginSelectorSpy.mockReturnValue(login)
-            testRender(<OrderComponent />)
-            expect(loginSelectorSpy).toHaveBeenCalled()
-            expect(screen.getByText('Order Page')).toBeTruthy()
-    })
-})
+describe("<OrderComponent />", () => {
+  it("shoud render Order page not crashing", () => {
+    jest.useFakeTimers();
+    testRender(<OrderComponent />);
+    setTimeout(() => {
+      expect(screen.getByText("Order Page")).toBeTruthy();
+    }, 1000);
+    jest.runAllTimers();
+  });
+  it("load login state in Order component", () => {
+    const login = {
+      isLoading: false,
+      username: "",
+      token: "",
+      error: "",
+    };
+    const loginSelectorSpy = jest.spyOn(redux, "useSelector");
+    loginSelectorSpy.mockReturnValue(login);
+    testRender(<OrderComponent />);
+    expect(loginSelectorSpy).toHaveBeenCalled();
+    expect(screen.getByText("Order Page")).toBeTruthy();
+  });
+});
